73394a9dfc8aa68a8fb303fbd0376412b952424c
Alexandre Fourcat <afourcat@gmail.com>
2022-12-14 01:10:01 +0100

    Rework day_5 parsing to make it simpler and add part2.

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,49 +1,106 @@
 #![allow(dead_code)]
 #![feature(iter_next_chunk)]
-use std::fmt::{Display, Formatter};
+use std::{
+    fmt::{Display, Formatter},
+    str::FromStr,
+};
 
-use anyhow::Context;
+use anyhow::{bail, Context};
 
 #[derive(Debug, Clone)]
 struct Stack {
-    inner: Vec<Vec<Option<char>>>,
+    height: usize,
+    width: usize,
+    inner: Vec<Vec<char>>,
 }
 
 impl Stack {
     fn new(input: &[&str]) -> anyhow::Result<Self> {
-        let height = input.len();
-        let len = ((input[0].len() - 1) / 4) + 1;
+        let max_stack_height = input.len();
+        let nb_stack = (input[0].len() + 1) / 4;
 
-        println!("creating {len} stack of height {height}");
-        let mut inner = (0..)
-            .take(len)
-            .map(|_| Vec::with_capacity(height))
-            .collect::<Vec<Vec<Option<char>>>>();
+        let mut stacks: Vec<Vec<char>> = vec![Vec::with_capacity(max_stack_height); nb_stack];
 
-        for (y, line) in input.iter().rev().enumerate() {
-            println!("line: {y}{line}");
-            let mut chars = line.chars().next_chunk::<4>().iter().enumerate();
-
-            loop {
-                match (a, b, c) {
-                    (' ', ' ', ' ') => inner[idx].push(None),
-                    ('[', x, ']') => inner[idx].push(Some(*x)),
-                    _ => anyhow::bail!("invalid input"),
-                }
-                chars.skip(1);
+        for line in input.iter().rev() {
+            let elems: Vec<char> = line.chars().collect();
+            for (x, chunk) in elems.chunks(4).enumerate() {
+                match chunk {
+                    ['[', c, ']', ' '] | ['[', c, ']'] => stacks[x].push(*c),
+                    [' ', ' ', ' ', ' '] | [' ', ' ', ' '] => (),
+                    _ => anyhow::bail!("Invalid input"),
+                };
             }
         }
 
-        Ok(Self { inner })
+        Ok(Self {
+            inner: stacks,
+            height: max_stack_height,
+            width: nb_stack,
+        })
+    }
+
+    fn r#move(&mut self, m: Movement) -> anyhow::Result<()> {
+        if m.from == m.to {
+            return Ok(());
+        } else if self.inner[m.from - 1].len() < m.nb {
+            anyhow::bail!(
+                "Cannot move {} from {} to {} no enough box",
+                m.nb,
+                m.from,
+                m.to
+            );
+        }
+
+        let mut from = std::mem::take(&mut self.inner[m.from - 1]);
+        self.inner[m.to - 1].extend_from_slice(&from[from.len() - m.nb..]);
+        from.truncate(from.len() - m.nb);
+        self.inner[m.from - 1] = from;
+
+        Ok(())
     }
 }
 
 impl Display for Stack {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         for (idx, stack) in self.inner.iter().enumerate() {
-            writeln!(f, "{} -> {}", idx, "flex")?;
+            writeln!(f, "{} -> {:?}", idx + 1, stack)?;
         }
         Ok(())
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct Movement {
+    nb: usize,
+    from: usize,
+    to: usize,
+}
+
+impl FromStr for Movement {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut splitted = s.split_whitespace().into_iter();
+        splitted.next();
+        let nb = splitted
+            .next()
+            .context("invalid movement")?
+            .parse::<usize>()
+            .context("not a number")?;
+        splitted.next();
+        let from = splitted
+            .next()
+            .context("invalid movement")?
+            .parse::<usize>()
+            .context("not a number")?;
+        splitted.next();
+        let to = splitted
+            .next()
+            .context("invalid movement")?
+            .parse::<usize>()
+            .context("not a number")?;
+
+        Ok(Movement { nb, from, to })
     }
 }
 
@@ -62,11 +119,23 @@ fn main() -> anyhow::Result<()> {
         .context("invalid input file, an empty line is required")?;
 
     let stacks_lines = &lines[..separator - 1];
-    let stack = Stack::new(stacks_lines).context("invalid stack input")?;
+    let mut stack = Stack::new(stacks_lines).context("invalid stack input")?;
     println!("{}", stack);
 
-    // let movements_lines = &lines[separator + 1..];
-    // println!("movements_lines: {:?}", movements_lines);
+    let movements = &lines[separator + 1..]
+        .iter()
+        .filter_map(|line| Movement::from_str(line).ok())
+        .collect::<Vec<Movement>>();
+
+    for m in movements {
+        stack.r#move(*m).context("failed to move")?;
+    }
+
+    println!("{}", stack);
+
+    for s in stack.inner.iter() {
+        print!("{}", s[s.len() - 1]);
+    }
 
     Ok(())
 }
